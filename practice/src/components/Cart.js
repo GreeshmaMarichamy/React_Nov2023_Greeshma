a136f24e61dc0377b776059a72c2b12199616a62
GreeshmaMarichamy <124855438+GreeshmaMarichamy@users.noreply.github.com>
2023-11-27 23:17:25 +0530

    Cart.js

--- practice/src/components/Cart.js
+++ practice/src/components/Cart.js
@@ -1,80 +1,72 @@
-import * as React from "react";
+import React,{useState} from "react";
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { Stack} from "@mui/material";
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-export default function Cart(){
+function Cart(){
+   const bull = (
+      <Box
+        component="span"
+        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+      >
+        •
+      </Box>
+    );
+    const[count,setCount]=useState(0);
+    function increment(){
+        var val=count;
+        setCount(++val);
+    }
+    function decrement(){
+        var valu=count;
+        setCount(--valu);
+    }
     return(
+    <div >
+      <h2>CART</h2>
+      <Card sx={{ minWidth: 275 , textAlign:"left"}}>
+      <CardContent style={{display:"flex"}}>
         <div>
-        <h1>CART</h1>
-{/*<Stack direction={{xs:"column",sm:"row"}} spacing={{xs:1,sm:2,md:4}} mt={5}>
-      <Card sx={{ maxWidth: 250 }}>
-         <CardMedia sx={{ height: 286 ,width:286}} image="https://cdn.shoplightspeed.com/shops/605879/files/59405456/400x400x2/lego-ahsoka-tanos-t-6-jedi-shuttle.jpg" />
-         <CardContent>
-           <p>AHSOKA TANO'S T-6 JEDI SHUTTLE</p>
-         </CardContent>
-         <CardActions>
-         <Button>ADD TO CART</Button>
-         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-         </CardActions>
-      </Card>
-      <Card sx={{ maxWidth: 250 }}>
-         <CardMedia sx={{ height: 286 ,width:286 }} image="https://cdn.shoplightspeed.com/shops/605879/files/59399242/400x400x2/lego-lego-star-wars-advent-calendar-2023.jpg" />
-         <CardContent>
-         <p>LEGO STAR WARS ADVENT CALENDAR 2023</p>
-         </CardContent>
-         <CardActions>
-         <Button>ADD TO CART</Button>
-         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-         </CardActions>
-      </Card>
-      <Card sx={{ maxWidth: 250 }}>
-         <CardMedia sx={{ height: 286 ,width:286}} image="https://cdn.shoplightspeed.com/shops/605879/files/59376413/400x400x2/lego-new-republic-e-wing-vs-shin-hatis-starfighter.jpg" />
-         <CardContent>
-         <p>NEW REPUBLIC E-WING VS. SHIN HATI'S STARFIGHTER</p>
-         </CardContent>
-         <CardActions>
-         <Button>ADD TO CART</Button>
-         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-         </CardActions>
-      </Card>
-      <Card sx={{ maxWidth: 250 }}>
-         <CardMedia sx={{height: 286 ,width:286}} image="https://cdn.shoplightspeed.com/shops/605879/files/59367713/400x400x2/lego-coruscant-guard-gunship.jpg" />
-         <CardContent>
-         <p>CORUSCANT GUARD GUNSHIP</p>
-         </CardContent>
-         <CardActions>
-         <Button>ADD TO CART</Button>
-         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-         </CardActions>
-      </Card>
-      <Card sx={{ maxWidth: 250 }}>
-         <CardMedia sx={{height: 286 ,width:286}} image="https://cdn.shoplightspeed.com/shops/605879/files/59363229/400x400x2/lego-chewbacca.jpg" />
-         <CardContent>
-         <p>CHEWBACCA</p>
-         </CardContent>
-         <CardActions>
-         <Button>ADD TO CART</Button>
-         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-         </CardActions>
-      </Card>
-      
-    </Stack>*/}
+        <Typography><img src="https://cdn.shoplightspeed.com/shops/605879/files/59060031/400x400x2/thames-kosmos-hydraulic-boxing-bots.jpg" width="150px" height="150px"></img>
+        </Typography>
+        </div>
+        <div style={{marginLeft:"80%"}}>
+        <Button onClick={increment} style={{textAlign:"right"}}>+</Button>
+        <h2 style={{textAlign:"right"}}>{count}</h2>
+        <Button onClick={decrement} style={{textAlign:"right"}}>-</Button>
+        </div>
+      </CardContent>
+    </Card>
+      <Card sx={{ minWidth: 275 , textAlign:"left"}}>
+      <CardContent style={{display:"flex"}}>
+        <div>
+        <Typography><img src="https://cdn.shoplightspeed.com/shops/605879/files/59060031/400x400x2/thames-kosmos-hydraulic-boxing-bots.jpg" width="150px" height="150px"></img>
+        </Typography>
+        </div>
+        <div style={{marginLeft:"80%"}}>
+        <Button onClick={increment} style={{textAlign:"right"}}>+</Button>
+        <h2 style={{textAlign:"right"}}>{count}</h2>
+        <Button onClick={decrement} style={{textAlign:"right"}}>-</Button>
+        </div>
+      </CardContent>
+    </Card>
+      <Card sx={{ minWidth: 275 , textAlign:"left"}}>
+      <CardContent style={{display:"flex"}}>
+        <div>
+        <Typography><img src="https://cdn.shoplightspeed.com/shops/605879/files/59060031/400x400x2/thames-kosmos-hydraulic-boxing-bots.jpg" width="150px" height="150px"></img>
+        </Typography>
+        </div>
+        <div style={{marginLeft:"80%"}}>
+        <Button onClick={increment} style={{textAlign:"right"}}>+</Button>
+        <h2 style={{textAlign:"right"}}>{count}</h2>
+        <Button onClick={decrement} style={{textAlign:"right"}}>-</Button>
+        </div>
+      </CardContent>
+    </Card>
     </div>
     );
 }
+export default Cart;
